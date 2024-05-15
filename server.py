1a2224432ce3030363718c9e52649fdedf07eda3
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-15 16:00:36 +0200

    :zap: read port from env else 8000

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
-
 import http.server
 import socketserver
-import sys
+import os
 
-PORT = 8000
+PORT = int(os.getenv("MOCK_PORT", 8000))
 DIRECTORY_TO_SERVE = "pages"
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY_TO_SERVE, **kwargs)
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), Handler, bind_and_activate=False) as httpd:
+    httpd.allow_reuse_address = True
     print("Serving at port", PORT)
     try:
+        httpd.server_bind()
+        httpd.server_activate()
         httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print("Closed.")
+    except:
+        httpd.server_close()
+        print("Closed.")
+        raise
